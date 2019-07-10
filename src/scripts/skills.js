f2197a19dd79814f7a8f2608d16785bf7303cfd6
Johnny Bravo <89266157778@mail.ru>
2019-07-10 23:15:47 +0300

    Работа на проверку

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -9,9 +9,17 @@ const skillName = {
   methods: {
     skillPersentScale() {
       const circle = this.$refs['circle'];
+      let findBlcTop = this.$root.findCircle();
       const dashArray = parseInt(getComputedStyle(circle).getPropertyValue("stroke-dasharray"));
       const persent = (dashArray/100) * (100 - this.skillPersent);
       circle.style.strokeDashoffset = persent;
+
+      window.addEventListener('scroll', function(){
+        const posTop = findBlcTop.findTop.getBoundingClientRect().top;
+        const fixTop = posTop.toFixed();
+        if (fixTop > 300 && fixTop < 350) {
+        }
+      });
     }
   },
   mounted() {
@@ -38,6 +46,14 @@ new Vue({
   data() {
     return {
       skills : []
+    }
+  },
+  methods: {
+    findCircle() {
+      let circleBlock = this.$refs["skills-block"];
+      return {
+        findTop: circleBlock
+      }
     }
   },
   created () {
