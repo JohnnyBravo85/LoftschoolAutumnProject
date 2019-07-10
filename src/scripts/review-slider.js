20df0071dad9408de76a10f8433ea468ea44049b
Johnny Bravo <89266157778@mail.ru>
2019-07-10 19:48:35 +0300

    Готовое ТЗ2 без линтера

--- src/scripts/review-slider.js
+++ src/scripts/review-slider.js
@@ -1,19 +1,10 @@
 import Vue from 'vue';
-
-const reviewsSliderControl = {
-  template: '#reviews-slider-control'
-}
+import Flickity from 'vue-flickity';
 
 const reviewsSliderContent = {
   template: '#reviews-slider-content',
-  data() {
-    return {
-      reviews : []
-    }
-  },
-  created() {
-    const data = require("../data/review-slider.json");
-    this.reviews = data;
+  props: {
+    review: Object
   }
 }
 
@@ -21,7 +12,39 @@ new Vue({
   el: '#reviews-slider-container',
   template: '#reviews-slider',
   components: {
-    reviewsSliderControl,
+    Flickity,
     reviewsSliderContent
+  },
+  data() {
+    return {
+      reviews : [],
+      flickityOptions: {
+        initialIndex: 0,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: false,
+        groupCells: true
+        // any options from Flickity can be used
+      }
     }
+  },
+  methods: {
+    makeArrayWithRequireAvatar(data) {
+      return data.map(item => {
+        const requireAvatar = require(`../images/content/${item.avatar}`);
+        item.avatar = requireAvatar;
+        return item
+      });
+    },
+    next() {
+      this.$refs.flickity.next();
+    },
+    previous() {
+      this.$refs.flickity.previous();
+    }
+  },
+  created() {
+    const data = require("../data/review-slider.json");
+    this.reviews = this.makeArrayWithRequireAvatar(data);
+  }
 })
